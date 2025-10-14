6a33f4f2d84080f0450ac2d52e94b4f0f593eb9c
Nadr Abou Moustafa <Nadr.AbouMoustafa@italtel.com>
2025-10-14 17:10:38 +0200

    nuova login

--- scripts/script.js
+++ scripts/script.js
@@ -1,5 +1,5 @@
 import { initializeApp } from 'https://www.gstatic.com/firebasejs/12.2.1/firebase-app.js'
-import { getAuth } from 'https://www.gstatic.com/firebasejs/12.2.1/firebase-auth.js'
+import { getAuth, signInWithEmailAndPassword, onAuthStateChanged } from 'https://www.gstatic.com/firebasejs/12.2.1/firebase-auth.js'
 import { getFirestore, collection, getDocs } from 'https://www.gstatic.com/firebasejs/12.2.1/firebase-firestore.js'
 
 
@@ -12,22 +12,43 @@ const firebaseConfig = {
     appId: "1:916764319171:web:79f3e4e6d649b6fcfe7176"
 };
 
+document.addEventListener('keypress', function (event) {
+    if (event.key === "Enter") {
+        event.preventDefault()
+        document.getElementById("loginBtn").click();
+    }
+}
+);
+document.getElementById('loginBtn').addEventListener('click', login);
 
-function login() {
+export function login() {
     const email = document.getElementsByName('email')[0].value;
     const password = document.getElementsByName('password')[0].value;
-    
+
     const app = initializeApp(firebaseConfig);
     const auth = getAuth(app);
     
     signInWithEmailAndPassword(auth, email, password)
         .then((userCredential) => {
-            const user = userCredential.user;
+            window.location.href = "home.html";
         })
         .catch((error) => {
             const errorCode = error.code;
             const errorMessage = error.message;
+            console.log(errorCode)
+            console.log(errorMessage)
         });
 }
 
-window.login = login;
+
+try {
+    console.log("prova")
+    onAuthStateChanged(window.auth, (user) => {
+        console.log(auth)
+        if (user) {
+            window.location.href = "index.html";
+        }
+    });
+} catch (error) {
+
+}
