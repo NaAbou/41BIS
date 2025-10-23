e51f88ca1c1a373aeb4a29ffbbe399d4984d007a
Nadr Abou Moustafa <Nadr.AbouMoustafa@italtel.com>
2025-10-23 15:37:28 +0200

    last tryù

--- bot.py
+++ bot.py
@@ -30,7 +30,7 @@ async def on_ready():
     messages = []
     async for message in channel.history(limit=None):#, after=two_weeks_ago):
         messages.append({
-            "author": await bot.fetch_user(message.author.id),
+            "author": str(await bot.fetch_user(message.author.id)),
             "content": message.clean_content,
             "timestamp": message.created_at.isoformat()
         })
